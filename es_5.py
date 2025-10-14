c56bf5116bc2153eace0189f45a8dfc257d66125
chiaraangeletti <chiara.angeletti@campusdavinci.edu.it>
2025-10-14 11:39:42 +0200

    ascissa massima

---
+++ es_5.py
@@ -0,0 +1,23 @@
+#Dato un insieme di 20 punti su un piano cartesiano,
+#random compresi nell'intervallo 0,10. Calcolare il
+#punto con ascissa massima o il punto con ordinata minima
+
+import random
+
+x=[]
+y=[]
+
+for i in range (0,20):
+    x.append(random.randint(0,10))
+    y.append(random.randint(0,10))
+
+#ascissa massima?
+#scorrere la lista delle x, trovare il massimo e salvare l'indice
+#visualizzare poi con una print il numero
+
+massimo=x[0]
+for i in range (0,20):
+    if x[i]>massimo:
+        massimo=x[i]
+        indicemax=y[i]
+print(massimo,indicemax)
