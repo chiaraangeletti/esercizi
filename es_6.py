9351b7983356c761f0d6565c17569a9c26d57808
chiaraangeletti <chiara.angeletti@campusdavinci.edu.it>
2025-12-02 11:30:08 +0100

    Add files via upload

---
+++ es_6.py
@@ -0,0 +1,28 @@
+#Scrivere una funzione che dati due numeri interi restituisca il maggiore
+
+#def nMaggiore (numero1,numero2):
+    #if numero1>numero2:
+        #return numero1
+    #else:
+        #return numero2
+    
+#uno=input("Inserire il primo numero")
+#uno=int(uno)
+#due=input("Inserire il secondo numero")
+#due=int(due)
+
+#risultato=nMaggiore(uno,due)
+#print(risultato)
+
+#Scrivere una procedura che dato un numero intero, stampi a video
+#se è pari o dispari
+
+#def pariODispari (numero):
+    #if numero%2==0:
+        #print("Il numero è pari.")
+    #else:
+        #print("Il numero è dispari.")
+    
+#n=input("Inserire il numero da verificare")
+#n=int(n)
+#pariODispari(n)
