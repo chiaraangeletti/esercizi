dddb026d5a1d2dbe45fa02a7799789b62f7742ba
chiaraangeletti <chiara.angeletti@campusdavinci.edu.it>
2026-02-05 09:52:14 +0100

    Add files via upload
    con ciclo while

--- es_pari_dispari.py
+++ es_pari_dispari.py
@@ -21,9 +21,18 @@ def calcolaPariDispari(lista):
     print(pari)
     print(dispari)
     
+def generaListaMentre(lista):
+    contatore=0
+    while contatore<10:
+        elemento=random.randint(-10,10)
+        lista.append(elemento)
+        contatore=contatore+1
+    
 if __name__=="__main__":
     myList=[]
+    myList2=[]
     generaLista(myList)
     #Quando ad una procedura gli passo un arrey(vettore), viene passato per riferimento ovvero che non gli viene
     #passata la copia del valore ma l'indirizzo di memoria di quella variabile.
     calcolaPariDispari(myList)
+    generaListaMentre(myList2)
