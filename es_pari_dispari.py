499641b81f1a0523aa9b7d93f47bbb97d9dce1e3
chiaraangeletti <chiara.angeletti@campusdavinci.edu.it>
2026-02-05 09:45:50 +0100

    Add files via upload

---
+++ es_pari_dispari.py
@@ -0,0 +1,29 @@
+#Data una lista di elementi random interi nell'intervallo -10,+10
+#calcolare il numero degli elementi pari e quelli dispari.
+#1)Creare una lista di 10 elementi random
+#2)Calcolare gli elementi pari e dispari
+
+import random
+
+def generaLista(lista):
+    for i in range(0,10):
+        elemento=random.randint(-10,10)
+        lista.append(elemento)
+        
+def calcolaPariDispari(lista):
+    pari=0
+    dispari=0
+    for elemento in lista: #iteratore
+        if elemento%2==0:
+            pari=pari+1
+        else:
+            dispari=dispari+1
+    print(pari)
+    print(dispari)
+    
+if __name__=="__main__":
+    myList=[]
+    generaLista(myList)
+    #Quando ad una procedura gli passo un arrey(vettore), viene passato per riferimento ovvero che non gli viene
+    #passata la copia del valore ma l'indirizzo di memoria di quella variabile.
+    calcolaPariDispari(myList)
