a585a7f5346e59536b4ccc7fb85e6b81aaa8be8c
chiaraangeletti <chiara.angeletti@campusdavinci.edu.it>
2026-02-10 11:34:54 +0100

    Add files via upload

---
+++ es_12.py
@@ -0,0 +1,36 @@
+#Data una serie di 10 misurazioni randomiche intere, comprese tra
+#due intervalli forniti da tastiera, produrre in output un file di testo che
+#abbia i valori, la media dei valori, il numero di valori sopra una certa soglia
+#fissata a massimo delle misurazioni meno 10.
+#1) Creare una lista di 10 misurazioni random (procedura)
+#2) fare la media dei valori della lista (funzione)
+#3) Calcolare la soglia (funzione)
+#4) Contare il numero di valori sopra la soglia (procedura)
+
+import random
+
+def creaLista(lista):
+    nUno=input("Inserire il primo estremo")
+    nUno=int(nUno)
+    nDue=input("Inserire il secondo estremo")
+    nDue=int(nDue)
+    for i in range (0,10):
+        if nDue>nUno:
+            elemento=random.randint(nUno,nDue)
+        else:
+            elemento=random.randint(nDue,nUno)
+        lista.append(elemento)
+        
+def media(lista):
+    sommaElementi=0
+    numeroElementi=0
+    for elemento in lista:
+        sommaElementi=sommaElementi+elemento
+    mediaElementi=sommaElementi/len(lista)
+    return(mediaElementi)
+        
+
+if __name__=="__main__":
+    listaM=[]
+    creaLista(listaM)
+    mediaValori=media(listaM)
