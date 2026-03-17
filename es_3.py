e7ed4ecb26b94c78e2c9bf77468fca89890c4d34
chiaraangeletti <chiara.angeletti@campusdavinci.edu.it>
2026-03-17 11:14:30 +0100

    Add files via upload

---
+++ es_3.py
@@ -0,0 +1,17 @@
+#Esercizio 3 verifica.
+def aggiungi_voto(registro, voto):
+    registro.append(voto)
+    print(registro)
+
+def media_registro(registro, arrotonda_a=0):
+    somma=0
+    for voto in registro:
+        somma=somma+voto
+    media=somma/len(registro)
+    media=round(media,arrotonda_a)
+    return(media)
+
+if __name__ =="__main__":
+    classe=[6,7,5,8]
+    aggiungi_voto(classe,9)
+    print(media_registro(classe, arrotonda_a=1))
